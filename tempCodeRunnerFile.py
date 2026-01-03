a8fe207c85d737fc7199f0bf418ec08b9bc92de2
Mayur Mahajan <malimayur2006@gmail.com>
2026-01-03 22:23:33 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-ef check(age):
-#     if age>=18:
-#         return "You are eligible to vote"
-#     else:
-#         return "You are not eligible to vote"
-# print(check(17))
-# print(check(18))    
-
